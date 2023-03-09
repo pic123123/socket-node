d47d5c6663d0100cdee0153dd44bf7c2df46b9e0
Joker <40009719+pic123123@users.noreply.github.com>
2023-03-09 13:05:32 +0900

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+app.enableCors({
+  origin: "http://localhost:3000", // 허락하고자 하는 요청 주소.
+  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.이거없으면 cors걸림
+});
+
 app.get("/api", (req, res) => {
   res.status(200).json({
     message: "Hello world",
